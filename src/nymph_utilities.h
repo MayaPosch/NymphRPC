120fd72edd9ee0fd7b99624946c76f1a836f89d6
Maya Posch <maya@nyanko.ws>
2021-11-28 11:46:11 +0100

    Clean up header.

--- src/nymph_utilities.h
+++ src/nymph_utilities.h
@@ -30,10 +30,8 @@ class NymphUtilities {
 	static std::string loggerName;
 	
 public:
-	//static UInt32 createHash(std::string name);
 	static int64_t getMessageId();
 	static NymphType* dynamicVarToNymphType(Poco::Dynamic::Var &object);
-	//static bool parseValue(uint8_t typecode, std::string* binmsg, int &index, NymphType* &value);
 	static bool parseValue(uint8_t typecode, uint8_t* binmsg, int &index, NymphType &value);
 };
 
